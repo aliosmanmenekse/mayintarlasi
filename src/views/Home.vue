f2f79df3d70ff387dd1eac10ff6ee81f26869899
Ali Osman Menekşe <ali.menekse@useinsider.com>
2020-02-07 21:10:56 +0300

    Mayın Tarlası

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" width="250" height="200">
+    <HelloWorld msg="Mayın Tarlası"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
